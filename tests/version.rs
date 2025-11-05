bd8fe074cca1ed5f1aadaa151284c3d218308484
Dominik Röttsches <drott@chromium.org>
2025-11-05 12:32:59 +0200

    Allow parsing of XML Version 1.x to improve conformance
    According to [1] an XML processor needs to accept documents
    with version strings matching the production:
    VersionNum::= '1.' [0-9]+
    
    [1] https://www.w3.org/TR/REC-xml/#sec-prolog-dtd
    
    Co-authored-by: Kornel <github@pornel.net>

---
+++ tests/version.rs
@@ -0,0 +1,39 @@
+#![forbid(unsafe_code)]
+
+use std::io::Cursor;
+
+use xml::common::XmlVersion;
+use xml::reader::XmlEvent;
+use xml::EventReader;
+
+mod util {
+    mod assert_match;
+}
+
+#[test]
+fn tolerated_versions() {
+    for version in ["1.2", "1.7", "1.1075", "1.000"] {
+        let source = format!(r#"<?xml version="{version}"?>"#);
+
+        let buf = Cursor::new(source);
+        let reader = EventReader::new(buf);
+        let mut it = reader.into_iter();
+
+        assert_match!(it.next(), Some(Ok(XmlEvent::StartDocument { version: XmlVersion::Version10, .. })));
+    }
+}
+
+#[test]
+fn rejected_xml_versions() {
+    let rejected_versions = ["1", "1.", "2.0", "1.0.0", "10", "1.0-", "100", "17.0"];
+
+    for version in rejected_versions {
+        let source = format!(r#"<?xml version="{version}"?>"#, version = version);
+
+        let buf = Cursor::new(source);
+        let reader = EventReader::new(buf);
+        let mut it = reader.into_iter();
+
+        assert!(format!("{:?}", it.next()).contains("Invalid XML version"));
+    }
+}
